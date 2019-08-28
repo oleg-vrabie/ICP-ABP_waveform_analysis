d7b78cf06cee0f164b0546149a8470262076b8b4
vrabiutz <syn8yk@gmail.com>
2019-08-28 15:12:03 +0200

    ...

---
+++ all_pca_clustering.py
@@ -0,0 +1,78 @@
+from sklearn.decomposition import PCA as PCA
+import scipy.io
+import numpy as np
+import inspect
+import sys
+sys.path.append('/home/ov/python/py_utils')
+import utils
+
+# Parameters:
+patient_list = [7, 10, 11, 12, 18, 23, 37]
+scid_list = ['scp']
+pwd = '/home/ov/preprocessed_waves/'
+
+pwd_concat = pwd + 'entire_nsc_data.npy'
+concatenated_waves = np.load(pwd_concat)
+
+print(concatenated_waves.shape)
+print(concatenated_waves.shape)
+
+# Normalization:
+from sklearn.preprocessing import normalize
+concatenated_waves = normalize(concatenated_waves, norm='max')
+
+# Fit-Transforming PCA on acquired waves:
+fitted = utils.pca_projections(concatenated_waves, 3, svd_solver='arpack')
+print(fitted.shape)
+
+xs = fitted[:, 0]
+ys = fitted[:, 1]
+zs = fitted[:, 2]
+
+scid = 'nsc'
+
+utils.plot_3d(xs, ys, zs,
+              title='PCA projection'.format(scid),
+              xlabel='PC1',
+              ylabel='PC2',
+              zlabel='PC3',
+              s=1)
+
+# Gaussian Mixture Models and, if needed, additional clustering
+n_components = int(input('Type the number of components to split into: '))
+
+cluster_means, clusters, ns, ks = utils.gaussian_mixture_pca_projections(
+                                     fitted, concatenated_waves,
+                                     n_components)
+
+
+"""
+clusters = np.asarray(clusters)
+
+# Cleaning (only if additional clustering was needed):
+if ns is not None:
+    print('\nCleaning...')
+    x = cluster_means.shape[0]
+
+    # Means:
+    cluster_means = np.delete(cluster_means, ns, axis=0)
+    # Clusters:
+    clusters = np.delete(clusters, ns, axis=0)
+
+    assert cluster_means.shape[0] == clusters.shape[0]
+    print('{} cluster(s) removed!'.format(x - cluster_means.shape[0]))
+
+# Save extracted clusters and their means:
+pwd_extracted = pwd + 'extracted/' +'nfpat' + str(patient_nr) + '.icp.normalized.' + scid
+pwd_means = pwd_extracted + 'normalized_means'
+pwd_clusters = pwd_extracted + '_clusters'
+assert cluster_means.shape[0] == clusters.shape[0]
+np.savetxt(pwd_means, cluster_means)
+np.save(pwd_clusters, clusters)
+
+print('\nMeans and clusters saved as: \n {} in {}\n {} in {}'.format(cluster_means.shape,
+                                                                     pwd_means,
+                                                                     clusters.shape,
+                                                                     pwd_clusters))
+
+"""
--- pca&clustering.py
+++ pca&clustering.py
@@ -7,7 +7,7 @@ sys.path.append('/home/ov/python/py_utils')
 import utils
 
 # Parameters:
-patient_nr = 18
+patient_nr = 7
 scid = 'nsc'
 pwd = '/home/ov/preprocessed_waves/'
 
@@ -30,7 +30,7 @@ concatenated_waves = normalize(concatenated_waves, norm='max')
 print(concatenated_waves.shape)
 
 # Fit-Transforming PCA on acquired waves:
-fitted = utils.pca_projections(concatenated_waves, 3, svd_solver='full')
+fitted = utils.pca_projections(concatenated_waves, 3, svd_solver='arpack')
 print(fitted.shape)
 
 xs = fitted[:, 0]
@@ -41,7 +41,8 @@ utils.plot_3d(xs, ys, zs,
               title='NFPAT{} {} PCA projection'.format(patient_nr, scid),
               xlabel='PC1',
               ylabel='PC2',
-              zlabel='PC3')
+              zlabel='PC3',
+              s=1)
 
 # Gaussian Mixture Models and, if needed, additional clustering
 n_components = int(input('Type the number of components to split into: '))
@@ -50,30 +51,35 @@ cluster_means, clusters, ns, ks = utils.gaussian_mixture_pca_projections(
                                      fitted, concatenated_waves,
                                      n_components, patient_nr, scid,
                                      with_mahal=False)
+
 clusters = np.asarray(clusters)
 
-# Cleaning (only if additional clustering was needed):
-if ns is not None:
-    print('\nCleaning...')
-    x = cluster_means.shape[0]
+if input('Continue with cleaning and saving? ') != 'yes':
+    import sys
+    sys.exit()
+else:
+    # Cleaning (only if additional clustering was needed):
+    if ns is not None:
+        print('\nCleaning...')
+        x = cluster_means.shape[0]
 
-    # Means:
-    cluster_means = np.delete(cluster_means, ns, axis=0)
-    # Clusters:
-    clusters = np.delete(clusters, ns, axis=0)
+        # Means:
+        cluster_means = np.delete(cluster_means, ns, axis=0)
+        # Clusters:
+        clusters = np.delete(clusters, ns, axis=0)
 
+        assert cluster_means.shape[0] == clusters.shape[0]
+        print('{} cluster(s) removed!'.format(x - cluster_means.shape[0]))
+
+    # Save extracted clusters and their means:
+    pwd_extracted = pwd + 'extracted/' +'nfpat' + str(patient_nr) + '.icp.normalized.' + scid
+    pwd_means = pwd_extracted + 'normalized_means'
+    pwd_clusters = pwd_extracted + '_clusters'
     assert cluster_means.shape[0] == clusters.shape[0]
-    print('{} cluster(s) removed!'.format(x - cluster_means.shape[0]))
+    np.savetxt(pwd_means, cluster_means)
+    np.save(pwd_clusters, clusters)
 
-# Save extracted clusters and their means:
-pwd_extracted = pwd + 'extracted/' +'nfpat' + str(patient_nr) + '.icp.' + scid
-pwd_means = pwd_extracted + '_means'
-pwd_clusters = pwd_extracted + '_clusters'
-assert cluster_means.shape[0] == clusters.shape[0]
-np.savetxt(pwd_means, cluster_means)
-np.save(pwd_clusters, clusters)
-
-print('\nMeans and clusters saved as: \n {} in {}\n {} in {}'.format(cluster_means.shape,
-                                                                     pwd_means,
-                                                                     clusters.shape,
-                                                                     pwd_clusters))
+    print('\nMeans and clusters saved as: \n {} in {}\n {} in {}'.format(cluster_means.shape,
+                                                                         pwd_means,
+                                                                         clusters.shape,
+                                                                         pwd_clusters))
